9bf1ac1cc03aa437823a430203dbe22abd836452
DU <121993195+rlaendud9880@users.noreply.github.com>
2023-03-17 17:29:38 +0900

    TodoList
    TodoList

---
+++ js/app.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+    AsideEvent.getInstance().addEventShowMenuButton();
+    AsideEvent.getInstance().addEventMainChange();
+    InformationService.getInstance().loadInfo();
+    InformationEvent.getInstance().addEventPhotoChangeClick();
+    InformationEvent.getInstance().addEventPhotoChange();
+    InformationEvent.getInstance().addEventAboutMeModifyClick();
+    InformationEvent.getInstance().addEventAboutMeSaveClick();
+    InformationEvent.getInstance().addEventIntroduceModifyClick();
+    InformationEvent.getInstance().addEventIntroduceSaveClick();
+    TodoService.getInstance();
+    TodoEvent.getInstance().addEventAddTodoClick();
+    TodoEvent.getInstance().addEventAddTodoKeyUp();
+
+}
